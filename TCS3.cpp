c5430fd35050056f60dc44193906ab8969473f5d
Shreya <yshreya178@gmail.com>
2026-07-14 23:57:09 +0530

    Add files via upload

---
+++ TCS3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main(){
+    int N=4;
+    int base_salary=5000;
+    for(int i=1;i<=N;i++){
+        cout<<i*base_salary<<" ";
+    }
+
+    return 0;
+
+}
